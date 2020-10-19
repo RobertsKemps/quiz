908e74d6cf19afd39ccd71b255c73448a45cc13e
RobertsKemps <robertskemps@gmail.com>
2020-10-19 21:48:40 +0300

    :card_file_box: add starting migrations

---
+++ database/migrations/2020_10_19_183840_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('questionId')->unsigned();
+            $table->text('text');
+            $table->boolean('isRight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
