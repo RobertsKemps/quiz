6e08a786119ba6f3586068f835afc80ecfaf12a6
RobertsKemps <robertskemps@gmail.com>
2020-10-20 22:07:20 +0300

    :construction: Create base route for quizDataUpdate

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuizDataUpdateController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/quizDataUpdate', [QuizDataUpdateController::class, 'update'])->name('quizDataUpdateRoute');
