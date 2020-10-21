ba03841748d05b16853d08e463401e3d4fcd5474
RobertsKemps <robertskemps@gmail.com>
2020-10-21 21:28:31 +0300

    Create command to update quiz data

---
+++ app/Console/Commands/QuizDataUpdateCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\QuizDataUpdateService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class QuizDataUpdateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'quiz:data:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command that calls QuizApi and updates all the data once a day';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $quizUpdateService = new QuizDataUpdateService();
+        $quizUpdateService->updateQuizData();
+        $response = Http::get('https://quizapi.io/api/v1/questions', [
+            'apiKey' => config('services.quizApi.key'),
+            'limit' => 10,
+        ]);
+
+        dd(json_decode($response->body()));
+
+        return 0;
+    }
+}
