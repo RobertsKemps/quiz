cbd10f247984e725b093556cdb9f5355aa0a01d7
RobertsKemps <robertskemps@gmail.com>
2020-10-24 21:25:17 +0300

    Move api call save methods to their according models

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'questionId',
+        'text',
+        'isRight',
+    ];
+
+    public static function saveAnswerFromApiRequest(object $response, Question $question)
+    {
+        $answerModel = new Answer();
+        foreach ($response->answers as $key => $answer) {
+            $correctAnswerKey = $key . '_correct';
+            if (!$correctAnswerKey) {
+                continue;
+            }
+
+            $isAnswerRight = $response->correct_answers->$correctAnswerKey;
+            $answerDataArray = [
+                'questionId' => $question->id,
+                'text' => $response->question,
+                'isRight' => filter_var($isAnswerRight, FILTER_VALIDATE_BOOLEAN),
+            ];
+
+            $answerModel->fill($answerDataArray);
+        }
+
+        $answerModel->save();
+    }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    /**
+     * Save and return question that was recieved from api request
+     *
+     * @param object $response
+     * @return self
+     */
+    public static function saveQuestionFromApiRequest(object $response, Category $category): self
+    {
+        $question = new Question();
+        $question->categoryId = $category->id;
+        $question->text = $response->question;
+        $question->save();
+
+        return $question;
+    }
 }
